43431b7673575150f14c9a6d777317436642d84a
Shana Nguyen <shananguyen01@gmail.com>
2021-01-22 20:30:54 -0800

    combined forward and backward into one graph; can set hip threshold level where lower hip angle values during stance phase (after HS but before TO) will trigger biofeedback

--- footAVyshankAVz.py
+++ footAVyshankAVz.py
@@ -48,14 +48,17 @@ def setParticipant(participant):
     
     #Columns are indexed from zero
     if (participant != 'p1401' and participant != 'p1402') :
+        footAVyColumnName = 'Right Foot y'
+        hipZXYFlexionColumnName = 'Right Hip Flexion/Extension'
         excel_shank_AVz = pandas.read_excel(trials[participant][FILEPATH_INDEX], sheet_name='Segment Angular Velocity', usecols=[51])
         excel_foot_AVy = pandas.read_excel(trials[participant][FILEPATH_INDEX], sheet_name='Segment Angular Velocity', usecols=[53])
-        footAVyColumnName = 'Right Foot y'
+        excel_hipZXY_flexion = pandas.read_excel(trials[participant][FILEPATH_INDEX], sheet_name='Joint Angles ZXY', usecols=[45])
+
     # Participant 14 uses the calculated AV
     else:
         excel_shank_AVz = pandas.read_excel(trials[participant][FILEPATH_INDEX], sheet_name='Segment Angular Velocity', usecols=[4])
     
-    return forwardStartRow, forwardEndRow, backwardStartRow, backwardEndRow, shankAVzColumnName, footAVyColumnName, excel_shank_AVz, excel_foot_AVy
+    return forwardStartRow, forwardEndRow, backwardStartRow, backwardEndRow, shankAVzColumnName, footAVyColumnName, hipZXYFlexionColumnName, excel_shank_AVz, excel_foot_AVy, excel_hipZXY_flexion
 
 
 
@@ -82,8 +85,10 @@ NOTES:
 (3) TO conditions: wait 300ms after HS; find foot AV max that is > 1
 
 '''
-def getEventsWithFootAVShankAngVel(row, lastRow, shankAVzColumnName, footAVyColumnName, excel_shank_AVz, excel_foot_AVy, 
-                                    frequency, direction, waitRow80, waitRow300, waitRow50) :
+def getEventsWithFootAVShankAngVel(row, lastRow, hipThreshold,
+                                   shankAVzColumnName, footAVyColumnName, hipZXYFlexionColumnName,
+                                   excel_shank_AVz, excel_foot_AVy, excel_hipZXY_flexion, 
+                                   frequency, direction, waitRow80, waitRow300, waitRow50) :
     
     previousAngularVelocity = -1000.0
     previousDifference = -1000.0
@@ -96,23 +101,27 @@ def getEventsWithFootAVShankAngVel(row, lastRow, shankAVzColumnName, footAVyColu
     TO = 1
     startTime = 0
     
-    data = { 'MSW': {'MSW Row': [], 'MSW Time' : [], 'MSW Angular Velocity' : []},
-    'HS' : {'HS Row': [], 'HS Time' : [], 'HS Angular Velocity' : []},
-    'TO' : {'TO Row': [], 'TO Time' : [], 'TO Angular Velocity' : []}
+    data = { 'MSW': {'Row': [], 'Time' : [], 'Angular Velocity' : []},
+    'HS' : {'Row': [], 'Time' : [], 'Angular Velocity' : []},
+    'TO' : {'Row': [], 'Time' : [], 'Angular Velocity' : []}
     }
     
-    allexcel = { 'row' : [], 'value' : [] }
+    #allexcel = { 'row' : [], 'value' : [] }
+    
+    hipData = { 'All Hip Values' : { 'Row' : [], 'Time' : [], 'Joint Angle' : [] },
+               'Crossed Threshold' : { 'Row' : [], 'Time' : [], 'Joint Angle' : [] }
+               }
+    
     
     dir = 1
     
     if(direction == 'backward') :
         dir = -1
         
+    biofeedbackStatus = 'OFF' 
     
     # programStartTime = time.time()
     while (row < lastRow):
-        
-        #HELLO
         
         row += 1
     
@@ -120,8 +129,13 @@ def getEventsWithFootAVShankAngVel(row, lastRow, shankAVzColumnName, footAVyColu
             
         footAV = excel_foot_AVy[footAVyColumnName].iloc[row] * dir
         
+        hipAngle = excel_hipZXY_flexion[hipZXYFlexionColumnName].iloc[row]
+        
         #allexcel['row'].append(row)
         #allexcel['value'].append(angularVelocity)
+        hipData['All Hip Values']['Row'].append(row)
+        hipData['All Hip Values']['Time'].append(row * (1/frequency))
+        hipData['All Hip Values']['Joint Angle'].append(hipAngle)
         
         ''' Start setup section '''
         
@@ -143,16 +157,40 @@ def getEventsWithFootAVShankAngVel(row, lastRow, shankAVzColumnName, footAVyColu
         
         footAVDifference = footAV - previousFootAV
             
-        ''' End setup section '''
+        
+        
+        
+        ''' Check hip angle '''
+        
+        # during stance phase, (after HS but before TO), check hip threshold
+        if HS == 1 :
+            
+            if hipAngle < hipThreshold and biofeedbackStatus == 'OFF' :
+                biofeedbackStatus = 'ON'
+                print("BIOFEEDBACK ", biofeedbackStatus, " - ", row)
+                hipData['Crossed Threshold']['Row'].append(row)
+                hipData['Crossed Threshold']['Time'].append(row * (1/frequency))
+                hipData['Crossed Threshold']['Joint Angle'].append(hipAngle)
+                
+            elif hipAngle < hipThreshold and biofeedbackStatus == 'ON' :
+                hipData['Crossed Threshold']['Row'].append(row)
+                hipData['Crossed Threshold']['Time'].append(row * (1/frequency))
+                hipData['Crossed Threshold']['Joint Angle'].append(hipAngle)
+                
+            elif hipAngle > hipThreshold and biofeedbackStatus == 'ON' :
+                biofeedbackStatus = 'OFF'
+                print("BIOFEEDBACK ", biofeedbackStatus, " - ", row)
+        
+
         
         
         # find minima + negative footAV for MSW
-        if(previousFootAVDifference < 0 and footAVDifference > 0 and footAV < 0 and TO == 1) :
+        if (previousFootAVDifference < 0 and footAVDifference > 0 and footAV < 0 and TO == 1) :
             
-            if(angularVelocity > 0) :
-                data['MSW']['MSW Row'].append(row - 1)
-                data['MSW']['MSW Time'].append((row - 1)*((1/frequency)))
-                data['MSW']['MSW Angular Velocity'].append(previousAngularVelocity)
+            if angularVelocity > 0 :
+                data['MSW']['Row'].append(row - 1)
+                data['MSW']['Time'].append((row - 1)*((1/frequency)))
+                data['MSW']['Angular Velocity'].append(previousAngularVelocity)
                 
                 MSW = 1
                 TO = 0
@@ -161,7 +199,7 @@ def getEventsWithFootAVShankAngVel(row, lastRow, shankAVzColumnName, footAVyColu
                 # check if there's a max within 50ms (arbitrary time interval)
                 startRow = row
                 notDone = True
-                while (row - startRow < waitRow50 and notDone) :
+                while row - startRow < waitRow50 and notDone :
                     row += 1
                     angularVelocity = excel_shank_AVz[shankAVzColumnName].iloc[row] * 180 / math.pi
                     footAV = excel_foot_AVy[footAVyColumnName].iloc[row] * dir
@@ -169,10 +207,10 @@ def getEventsWithFootAVShankAngVel(row, lastRow, shankAVzColumnName, footAVyColu
                     difference = angularVelocity - previousAngularVelocity
                     footAVDifference = footAV - previousFootAV
          
-                    if (angularVelocity > 0) :
-                        data['MSW']['MSW Row'].append(row - 1)
-                        data['MSW']['MSW Time'].append((row - 1)*((1/frequency)))
-                        data['MSW']['MSW Angular Velocity'].append(previousAngularVelocity)
+                    if angularVelocity > 0 :
+                        data['MSW']['Row'].append(row - 1)
+                        data['MSW']['Time'].append((row - 1)*((1/frequency)))
+                        data['MSW']['Angular Velocity'].append(previousAngularVelocity)
                         
                         MSW = 1
                         TO = 0
@@ -183,76 +221,126 @@ def getEventsWithFootAVShankAngVel(row, lastRow, shankAVzColumnName, footAVyColu
                     previousAngularVelocity = angularVelocity
                     previousDifference = difference
                     
-                continue
-            
-            previousFootAV = footAV
-            previousFootAVDifference = footAVDifference
-            previousAngularVelocity = angularVelocity
-            previousDifference = difference
-                    
-            continue
-    
+                continue # continue so that the 'previous' values aren't updated again
             
     
         # finding HS with Foot AV
-        if MSW == 1:
+        elif MSW == 1:
             
             # find a maxima with positive foot AV
             if previousFootAVDifference > 0 and footAVDifference < 0 and footAV > 0:
-                data['HS']['HS Row'].append(row - 1)
-                data['HS']['HS Time'].append((row - 1)*(1/frequency))
-                data['HS']['HS Angular Velocity'].append(previousAngularVelocity)
+                data['HS']['Row'].append(row - 1)
+                data['HS']['Time'].append((row - 1)*(1/frequency))
+                data['HS']['Angular Velocity'].append(previousAngularVelocity)
                 
                 startTime = row
                 HS = 1
                 MSW = 0
-                    
-            previousFootAV = footAV
-            previousFootAVDifference = footAVDifference
-            previousAngularVelocity = angularVelocity
-            previousDifference = difference
-            continue
         
         
         # finding TO with Foot AV
-        if HS == 1:
+        elif HS == 1:
                 
             currentTime = row - startTime
             
-            if currentTime > waitRow300 :
-            
-                # find a maxima and positive foot AV
-                if previousFootAVDifference > 0 and footAVDifference < 0 and footAV > 1:
-                    data['TO']['TO Row'].append(row - 1)
-                    data['TO']['TO Time'].append((row - 1)*(1/frequency))
-                    data['TO']['TO Angular Velocity'].append(angularVelocity)
+            # after 300ms, find a maxima and positive foot AV
+            if currentTime > waitRow300 and previousFootAVDifference > 0 and footAVDifference < 0 and footAV > 1:
                     
-                    TO = 1
-                    HS = 0
-                   
-            previousFootAV = footAV
-            previousFootAVDifference = footAVDifference
-            previousAngularVelocity = angularVelocity
-            previousDifference = difference
-            continue
+                if biofeedbackStatus == 'ON' :
+                    biofeedbackStatus = 'OFF'
+                    print("BIOFEEDBACK ", biofeedbackStatus, " - ", row)
+                    
+                data['TO']['Row'].append(row - 1)
+                data['TO']['Time'].append((row - 1)*(1/frequency))
+                data['TO']['Angular Velocity'].append(angularVelocity)
+                
+                TO = 1
+                HS = 0
+                  
             
         previousFootAV = footAV
         previousFootAVDifference = footAVDifference
         previousAngularVelocity = angularVelocity
         previousDifference = difference
         
-    return data, allexcel
+    return data, hipData
     
     
     
     
     
     
+'''
+Graphs for one direction
+
+Inputs:
+    - data: a dictionary that holds dictionary values
+    e.g.) { 'MSW' : { 'Row' : [0,1,2..], 'Time' : [...] },
+           'HS' : { 'Row' : [0,1,2..], 'Time' : [...] } }
+    - see documentation for graphCombined(...) for other input parameters
+'''
+def graph(data, forwardEnd, backwardStart, participantName, title, xLabel, yLabel, dataTypesNames, columnNames, colors) :
+    
+    fig=plt.figure()
+    ax=fig.add_axes([0,0,1,1])
+    
+    # adding backward values to forward values
+    for i in range(len(dataTypesNames)) :
+        
+        # print the gait events
+        if len(dataTypesNames) == 3:
+            print( dataTypesNames[i], "\n", DataFrame(data[dataTypesNames[i]], columns = columnNames) )
+            
+        ax.scatter(data[dataTypesNames[i]][columnNames[1]], data[dataTypesNames[i]][columnNames[2]], color=colors[i])
+    
+    ax.set_xlabel(xLabel)
+    ax.set_ylabel(yLabel)
+    ax.set_title(participantName + " " +  title)
+    plt.axvline(x = (forwardEnd + backwardStart) / 2)
+    plt.show()
     
     
     
+'''
+Graphs both forward and backward
+
+Inputs:
+    - allData: an array of length 2 with dictionary values that hold a dictionary
+    e.g.) allData = [ {
+                        'MSW' : { 'Row' : [0,1,2..], 'Time' : [...] },
+                        'HS' : { 'Row' : [0,1,2..], 'Time' : [...] } 
+                      }, # forward values
+                      {
+                        'MSW' : { 'Row' : [0,1,2..], 'Time' : [...] },
+                        'HS' : { 'Row' : [0,1,2..], 'Time' : [...] } 
+                      } # backward values
+                        ]
+    - forwardEnd: the ending time of the forward phase
+    - backwardStart: the start time of the backward phase
+    - title: title of the graph
+    - xLabel: label for the x-axis
+    - yLabel: label for the y-axis
+    - dataTypesNames: array of 'parent' dictionary keys (e.g. MSW and HS above)
+        - 2nd value will be the x-axis and 3rd value will be the y-axis
+    - columnNames: array of 'child' dictionary keys (e.g. Row and Time above)
+    - colors: array of color values
+
+'''
+def graphCombined(allData, forwardEnd, backwardStart, participantName, title, xLabel, yLabel, dataTypesNames, columnNames, colors) :
+    
+    combinedData = allData[0]
+
+    # adding backward values to forward values
+    for i in range(len(dataTypesNames)) :
+        
+        for columnName in columnNames : 
+            
+            combinedData[dataTypesNames[i]][columnName].extend(allData[1][dataTypesNames[i]][columnName])
+    
+    graph(combinedData, forwardEnd, backwardStart, participantName, title, xLabel, yLabel, dataTypesNames, columnNames, colors)
     
     
+
 '''
 Frequency at which data is taken:
 Participant 4 = 60 Hz
@@ -261,15 +349,22 @@ Participant 28 = 100Hz
 Participant 3 = 60 Hz
 '''
 
-def main(participantName, frequency, title):
+def main(participantName, frequency, hipThreshold):
     
-    forwardStartRow, forwardEndRow, backwardStartRow, backwardEndRow, shankAVzColumnName, footAVyColumnName, excel_shank_AVz, excel_foot_AVy = setParticipant(participantName)
+    forwardStartRow, forwardEndRow, backwardStartRow, backwardEndRow, shankAVzColumnName, footAVyColumnName, hipZXYFlexionColumnName, excel_shank_AVz, excel_foot_AVy, excel_hipZXY_flexion = setParticipant(participantName)
     waitRow80 = convertMilliSecToRow(frequency, 80)
     waitRow300 = convertMilliSecToRow(frequency, 300)
     waitRow50 = convertMilliSecToRow(frequency, 50)
     
-    directions = ['forward', 'backward']
+    gaitEventsTitle = 'Shank AV z'
+    bothGaitDirections = []
     
+    hipTitle = 'Hip ZXY Flexion/Extension'
+    allHipData = []
+    
+    forwardEnd = 0
+    backwardStart = 0
+    directions = ['forward', 'backward']
     
     for i in range(2) :
         direction = directions[i]
@@ -279,39 +374,35 @@ def main(participantName, frequency, title):
         if i == 0 :
             startRow = forwardStartRow
             endRow = forwardEndRow
+            forwardEnd = endRow
         else :
             startRow = backwardStartRow
             endRow = backwardEndRow
+            backwardStart = startRow
             
         print('\n', direction.capitalize())
       
-        gaitEvents = getEventsWithFootAVShankAngVel(startRow, endRow, 
-                                                       shankAVzColumnName, footAVyColumnName, 
-                                                       excel_shank_AVz, excel_foot_AVy, 
+        gaitEvents, hipData = getEventsWithFootAVShankAngVel(startRow, endRow, hipThreshold,
+                                                       shankAVzColumnName, footAVyColumnName, hipZXYFlexionColumnName,
+                                                       excel_shank_AVz, excel_foot_AVy, excel_hipZXY_flexion, 
                                                        frequency, direction,
                                                        waitRow80, waitRow300, waitRow50)
+        bothGaitDirections.append(gaitEvents)
+        allHipData.append(hipData)
+        
+    # graph gait events
+    graphCombined(bothGaitDirections, forwardEnd * (1/frequency), backwardStart * (1/frequency), 
+                  participantName, gaitEventsTitle, 
+                  'Time (s)', 'Shank AV z', 
+                  ['MSW', 'HS', 'TO'], 
+                  ['Row', 'Time', 'Angular Velocity'], ['r', 'g', 'b'])
     
-        MSWdf = DataFrame(gaitEvents[0]['MSW'],columns=['MSW Row', 'MSW Time', 'MSW Angular Velocity'])
-        print (MSWdf)
-        
-        HSdf = DataFrame(gaitEvents[0]['HS'],columns=[ 'HS Row', 'HS Time', 'HS Angular Velocity'])
-        print (HSdf)
-        
-        TOdf = DataFrame(gaitEvents[0]['TO'],columns=[ 'TO Row', 'TO Time', 'TO Angular Velocity'])
-        print (TOdf)
-        
-        fig=plt.figure()
-        ax=fig.add_axes([0,0,1,1])
-        ax.scatter(gaitEvents[0]['MSW'][ 'MSW Time'], gaitEvents[0]['MSW']['MSW Angular Velocity'], color='r')
-        ax.scatter(gaitEvents[0]['HS'][ 'HS Time'], gaitEvents[0]['HS']['HS Angular Velocity'], color='b')
-        ax.scatter(gaitEvents[0]['TO'][ 'TO Time'], gaitEvents[0]['TO']['TO Angular Velocity'], color='g')
-        
-        ax.set_xlabel('Time')
-        ax.set_ylabel('AngularVelocity')
-        ax.set_title(participantName + direction.capitalize() + title)
-        plt.show()
-    
-
+    # graph hip angles
+    graphCombined(allHipData, forwardEnd * (1/frequency), backwardStart * (1/frequency), 
+                  participantName, hipTitle, 
+                  'Time (s)', 'Hip ZXY Flexion/Extension', 
+                  ['All Hip Values', 'Crossed Threshold'], 
+                  ['Row', 'Time', 'Joint Angle'], ['g', 'b'])
 
 
 
@@ -321,31 +412,27 @@ def main(participantName, frequency, title):
 
 if __name__ == "__main__":
     
-    #print("60 hz to 300ms = ", convertMilliSecToRow(60, 300), "100 hz to 300ms = ", convertMilliSecToRow(100, 300))
+    hipThreshold = -10
     
-    title = ': MSW uses footAV Y < 0 + footAV Y min w/ shank AV > 0 within 50ms'
-    
-    print('\nPARTICIPANT 4-01\n')
-    main('p401', 60, title)
+    #print('\nPARTICIPANT 4-01\n')
+    #main('p401', 60, hipThreshold)
     
     print('\nPARTICIPANT 4-02\n')
-    main('p402', 60, title)
+    main('p402', 60, hipThreshold)
     
     print('\nPARTICIPANT 28-01\n')
-    main('p2801', 100, title)
+    main('p2801', 100, hipThreshold)
     
     print('\nPARTICIPANT 28-02\n')
-    main('p2802', 100, title)
+    main('p2802', 100, hipThreshold)
     
     print('\nPARTICIPANT 3-03\n')
-    main('p303', 60, title)
+    main('p303', 60, hipThreshold)
     
     print('\nPARTICIPANT 31-03\n')
-    main('p3103', 100, title)
-    
+    main('p3103', 100, hipThreshold)
     
     '''
     ^ CHECK (1) PARTICIPANT NAME, 
-            (2) FREQUENCY, 
-            (3) TITLE ABOVE
+            (2) FREQUENCY
     '''
